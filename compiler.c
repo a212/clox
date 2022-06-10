1ccb1389b3089c91fac04abd23c92d8635ca7007
Alexey Yesipenko <alex7y@gmail.com>
2022-06-10 17:12:24 +0300

    calls and functions(24)

--- compiler.c
+++ compiler.c
@@ -45,7 +45,17 @@ typedef struct {
 	int depth;
 } Local;
 
-typedef struct {
+
+typedef enum {
+	TYPE_FUNCTION,
+	TYPE_SCRIPT,
+} FunctionType;
+
+typedef struct Compiler {
+	struct Compiler* enclosing;
+	ObjFunction* function;
+	FunctionType type;
+
 	Local locals[UINT8_COUNT];
 	int localCount;
 	int scopeDepth;
@@ -56,13 +66,14 @@ static bool match(TokenType type);
 static void declaration();
 static void varDeclaration();
 static void statement();
+static void initCompiler(Compiler* compiler, FunctionType type);
+static bool check(TokenType type);
 
 Parser parser;
 Compiler* current = NULL;
-Chunk *compilingChunk;
 
 static Chunk* currentChunk() {
-	return compilingChunk;
+	return &current->function->chunk;
 }
 
 static void errorAt(Token *token, const char *message) {
@@ -74,7 +85,7 @@ static void errorAt(Token *token, const char *message) {
 		fprintf(stderr, " at end");
 	} else if (token->type == TOKEN_ERROR) {
 	} else {
-		fprintf(stderr, " at %.s", token->length, token->start);
+		fprintf(stderr, " at %.*s", token->length, token->start);
 	}
 	fprintf(stderr, ": %s\n", message);
 	parser.hadError = true;
@@ -118,16 +129,20 @@ static void emitBytes(uint8_t byte1, uint8_t byte2) {
 }
 
 static void emitReturn() {
+	emitByte(OP_NIL);
 	emitByte(OP_RETURN);
 }
 
-static void endCompiler() {
+static ObjFunction* endCompiler() {
 	emitReturn();
+	ObjFunction* function = current->function;
 #ifdef DEBUG_PRINT_CODE
 	if (!parser.hadError) {
-		disassembleChunk(currentChunk(), "code");
+		disassembleChunk(currentChunk(), function->name != NULL ? function->name->chars : "script");
 	}
 #endif
+	current = current->enclosing;
+	return function;
 }
 
 static uint8_t makeConstant(Value value) {
@@ -308,8 +323,28 @@ static void or_(bool canAssign) {
 	patchJump(endJump);
 }
 
+static uint8_t argumentList() {
+	uint8_t argCount = 0;
+	if (!check(TOKEN_RIGHT_PAREN)) {
+		do {
+			expression();
+			if (argCount == 255) {
+				error("Can't have more than 255 arguments.");
+			}
+			argCount++;
+		} while (match(TOKEN_COMMA));
+	}
+	consume(TOKEN_RIGHT_PAREN, "Expect ')' after arguments.");
+	return argCount;
+}
+
+static void call(bool canAssign) {
+	uint8_t argCount = argumentList();
+	emitBytes(OP_CALL, argCount);
+}
+
 ParseRule rules[] = {
-  [TOKEN_LEFT_PAREN]    = {grouping, NULL,   PREC_NONE},
+  [TOKEN_LEFT_PAREN]    = {grouping, call,   PREC_CALL},
   [TOKEN_RIGHT_PAREN]   = {NULL,     NULL,   PREC_NONE},
   [TOKEN_LEFT_BRACE]    = {NULL,     NULL,   PREC_NONE}, 
   [TOKEN_RIGHT_BRACE]   = {NULL,     NULL,   PREC_NONE},
@@ -367,13 +402,13 @@ static bool match(TokenType type) {
 
 static void printStatement() {
 	expression();
-	consume(TOKEN_SEMICOLON, "Expect: ';' after value.");
+	consume(TOKEN_SEMICOLON, "Expect ';' after value.");
 	emitByte(OP_PRINT);
 }
 
 static void expressionStatement() {
 	expression();
-	consume(TOKEN_SEMICOLON, "Expect: ';' after expression.");
+	consume(TOKEN_SEMICOLON, "Expect ';' after expression.");
 	emitByte(OP_POP);
 }
 
@@ -477,11 +512,27 @@ static void forStatement() {
 	endScope();
 }
 
+static void returnStatement()
+{
+	if (current->type == TYPE_SCRIPT) {
+		error("Can't return from top-level code.");
+	}
+
+	if (match(TOKEN_SEMICOLON)) {
+		return emitReturn();
+	}
+	expression();
+	consume(TOKEN_SEMICOLON, "Expect ';' after return value.");
+	emitByte(OP_RETURN);
+}
+
 static void statement() {
 	if (match(TOKEN_PRINT)) {
 		printStatement();
 	} else if (match(TOKEN_IF)) {
 		ifStatement();
+	} else if (match(TOKEN_RETURN)) {
+		returnStatement();
 	} else if (match(TOKEN_WHILE)) {
 		whileStatement();
 	} else if (match(TOKEN_FOR)) {
@@ -553,6 +604,7 @@ static uint8_t parseVariable(const char* errorMessage) {
 }
 
 static void markInitialized() {
+	if (current->scopeDepth == 0) return;
 	current->locals[current->localCount - 1].depth = current->scopeDepth;
 }
 
@@ -572,12 +624,45 @@ static void varDeclaration() {
 	} else {
 		emitByte(OP_NIL);
 	}
-	consume(TOKEN_SEMICOLON, "Expect: ';' after variable declaration.");
+	consume(TOKEN_SEMICOLON, "Expect ';' after variable declaration.");
+	defineVariable(global);
+}
+
+static void function(FunctionType type) {
+	Compiler compiler;
+	initCompiler(&compiler, type);
+	beginScope();
+
+	consume(TOKEN_LEFT_PAREN, "Expect '(' after function name.");
+	if (!check(TOKEN_RIGHT_PAREN)) {
+		do {
+			current->function->arity++;
+			if (current->function->arity > 255) {
+				errorAtCurrent("Can't have more than 255 parameters.");
+			}
+			uint8_t constant = parseVariable("Expect parameter name.");
+			defineVariable(constant);
+		} while (match(TOKEN_COMMA));
+	}
+	consume(TOKEN_RIGHT_PAREN, "Expect ')' after parameters.");
+	consume(TOKEN_LEFT_BRACE, "Expect '{' before function body.");
+	block();
+
+	ObjFunction* function = endCompiler();
+	emitBytes(OP_CONSTANT, makeConstant(OBJ_VAL(function)));
+}
+
+static void funDeclaration() {
+	uint8_t global = parseVariable("Expect function name.");
+	markInitialized();
+	function(TYPE_FUNCTION);
 	defineVariable(global);
 }
 
 static void declaration() {
-	if (match(TOKEN_VAR)) {
+	if (match(TOKEN_FUN)) {
+		funDeclaration();
+	} else if (match(TOKEN_VAR)) {
 		varDeclaration();
 	} else {
 		statement();
@@ -585,18 +670,29 @@ static void declaration() {
 	if (parser.panicMode) synchronize();
 }
 
-static void initCompiler(Compiler* compiler)
+static void initCompiler(Compiler* compiler, FunctionType type)
 {
+	compiler->enclosing = current;
+	compiler->function = NULL;
+	compiler->type = type;
 	compiler->localCount = 0;
 	compiler->scopeDepth = 0;
+	compiler->function = newFunction();
 	current = compiler;
+	if (type != TYPE_SCRIPT) {
+		current->function->name = copyString(parser.previous.start, parser.previous.length);
+	}
+
+	Local* local = &current->locals[current->localCount++];
+	local->depth = 0;
+	local->name.start = "";
+	local->name.length = 0;
 }
 
-bool compile(const char *source, Chunk *chunk) {
+ObjFunction* compile(const char *source) {
 	initScanner(source);
 	Compiler compiler;
-	initCompiler(&compiler);
-	compilingChunk = chunk;
+	initCompiler(&compiler, TYPE_SCRIPT);
 
 	parser.hadError = false;
 	parser.panicMode = false;
@@ -607,7 +703,7 @@ bool compile(const char *source, Chunk *chunk) {
 		declaration();
 	}
 
-	endCompiler();
-	return !parser.hadError;
+	ObjFunction* function = endCompiler();
+	return parser.hadError ? NULL : function;
 }
 
--- object.c
+++ object.c
@@ -37,7 +37,21 @@ static uint32_t hashString(const char* key, int length) {
 	return hash;
 }
 
-ObjString *takeString(char* chars, int length) {
+ObjFunction* newFunction() {
+	ObjFunction* function = ALLOCATE_OBJ(ObjFunction, OBJ_FUNCTION);
+	function->arity = 0;
+	function->name = NULL;
+	initChunk(&function->chunk);
+	return function;
+}
+
+ObjNative* newNative(NativeFn function) {
+	ObjNative* native = ALLOCATE_OBJ(ObjNative, OBJ_NATIVE);
+	native->function = function;
+	return native;
+}
+
+ObjString* takeString(char* chars, int length) {
 	uint32_t hash = hashString(chars, length);
 	ObjString* interned = tableFindString(&vm.strings, chars, length, hash);
 	if (interned != NULL) {
@@ -47,7 +61,7 @@ ObjString *takeString(char* chars, int length) {
 	return allocateString(chars, length, hash);
 }
 
-ObjString *copyString(const char* chars, int length) {
+ObjString* copyString(const char* chars, int length) {
 	uint32_t hash = hashString(chars, length);
 	ObjString* interned = tableFindString(&vm.strings, chars, length, hash);
 	if (interned != NULL) return interned;
@@ -57,8 +71,22 @@ ObjString *copyString(const char* chars, int length) {
 	return allocateString(heapChars, length, hash);
 }
 
+static void printFunction(ObjFunction* function) {
+	if (function->name == NULL) {
+		printf("<script>");
+		return;
+	}
+	printf("<fn %s>", function->name->chars);
+}
+
 void printObject(Value value) {
 	switch (OBJ_TYPE(value)) {
+		case OBJ_FUNCTION:
+			printFunction(AS_FUNCTION(value));
+			break;
+		case OBJ_NATIVE:
+			printf("<native fn>");
+			break;
 		case OBJ_STRING:
 			printf("%s", AS_CSTRING(value));
 			break;
